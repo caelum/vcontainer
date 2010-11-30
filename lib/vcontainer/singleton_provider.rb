a0a8a06b57fec4cc6931e13f7456b3a7468a0597
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-11-30 14:05:30 -0200

    using provide for method on singleton components

--- lib/vcontainer/singleton_provider.rb
+++ lib/vcontainer/singleton_provider.rb
@@ -7,12 +7,18 @@ module VContainer
       @type = build_type
     end
     
-    def build
-      @cached_instance ||= @type.new
+    def build(container = NilContainer.new)
+      @cached_instance ||= instantiate(container)
     end
 
     def can_handle?(what)
       @type == what.to_s.camelize.constantize
+    end
+    
+    private
+    def instantiate(container)
+      values = container.provide_for_method(@type.instance_method(:initialize))
+      @type.new *values
     end
 
   end
